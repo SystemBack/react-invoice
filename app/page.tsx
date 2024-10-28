b29dcabce1cb0ef5682149e63b76ce76c13e2c4c
Eduardo <eduardo.cerv.jua@gmail.com>
2024-10-28 11:12:34 -0600

    chore: Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">
+        Invoicepedia
+      </h1>
+      <p>
+        <a href="">
+          Sing in
+        </a>
+      </p>
+    </main>
+  );
+}
